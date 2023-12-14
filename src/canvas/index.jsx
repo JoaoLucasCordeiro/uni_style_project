f11088ff110993e6b838c9868dd1e419b9b7956e
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-12-14 14:15:41 -0300

    feat: Shirt model was applied

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,25 +1,22 @@
-import { Canvas } from "@react-three/fiber"
-import {Environment, Center} from "@react-three/drei"
-import Shirt from "./Shirt"
-import BackDrop from "./Backdrop"
-import CameraRig from "./CameraRig"
-
-
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
+import Shirt from "./Shirt";
+import BackDrop from "./Backdrop";
+import CameraRig from "./CameraRig";
 
 const CanvasModel = () => {
   return (
-   <Canvas>
-    <ambientLight intensity={0.5}/>
-    <Environment preset="city"/>
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
 
+      <CameraRig>
+        <Center>
+          <Shirt />
+        </Center>
+      </CameraRig>
+    </Canvas>
+  );
+};
 
-  
-      <Center>
-        <Shirt/>
-      </Center>
-   
-   </Canvas>
-  )
-}
-
-export default CanvasModel
+export default CanvasModel;
